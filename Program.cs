d74683357b957a69112276f4b2bc6887f73c13da
Alexander McCutcheon <snowpuppy4290@gmail.com>
2025-05-16 13:44:17 -0500

    fix: Rollback all cors changes

--- Program.cs
+++ Program.cs
@@ -30,27 +30,13 @@ builder.Services.AddScoped<UserMappingService>();
 // Register ProgressTracker
 builder.Services.AddSingleton<IProgressTracker, ProgressTracker>();
 
-// Configure CORS
 builder.Services.AddCors(options =>
 {
-    var allowedOrigins = builder.Environment.IsDevelopment()
-        ? new[] { "*" }
-        : (Environment.GetEnvironmentVariable("FRONTEND_URLS") ?? "").Split(';', StringSplitOptions.RemoveEmptyEntries);
-
-    options.AddPolicy("CustomCorsPolicy", policy =>
+    options.AddDefaultPolicy(policy =>
     {
-        if (builder.Environment.IsDevelopment())
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        }
-        else
-        {
-            policy.WithOrigins(allowedOrigins)
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        }
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
@@ -63,8 +49,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Use CORS
-app.UseCors("CustomCorsPolicy");
+app.UseCors();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
